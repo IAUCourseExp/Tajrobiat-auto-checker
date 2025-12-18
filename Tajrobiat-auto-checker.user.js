d2b98813ea62e74c2fe86cb4d3fbd22d78ffd230
IAUCourseExp <lexfridman1917@gmail.com>
2025-12-18 22:30:36 +0330

    Update Tajrobiat-auto-checker.user.js

--- Tajrobiat-auto-checker.user.js
+++ Tajrobiat-auto-checker.user.js
@@ -1,49 +1,20 @@
 // ==UserScript==
 // @name         Auto checker (Amoozeshyar form helper)
 // @namespace    http://tampermonkey.net/
-// @version      1.1
-// @description  Automatically selects "خیلی خوب" in Professors survey forms
+// @version      1.0
+// @description  Automatically selects the right option for Professors survey forms
 // @author       @IAUCourseExp
-// @match        *://amoozeshyar.iau.ir/*
+// @match        *://*/*
 // @grant        none
 // ==/UserScript==
 
 (function () {
     'use strict';
-
-    const TARGET_VALUE = "7150024486";
-
-    function checkAllRadios() {
-        const radios = document.querySelectorAll(
-            `input[type="radio"][value="${TARGET_VALUE}"]`
-        );
-
-        if (radios.length === 0) return false;
-
-        radios.forEach(radio => {
-            if (!radio.checked) {
-                radio.checked = true;
-
-                const onclickAttr = radio.getAttribute("onclick");
-                if (onclickAttr) {
-                    eval(onclickAttr);
-                }
-
-                radio.dispatchEvent(new Event('change', { bubbles: true }));
-            }
-        });
-
-        return true;
-    }
-
-    const observer = new MutationObserver(() => {
-        if (checkAllRadios()) {
-            observer.disconnect();
+    document.querySelectorAll('input[type="radio"][value="7150024486"]').forEach(radio => {
+        radio.checked = true;
+        const onclickAttr = radio.getAttribute("onclick");
+        if (onclickAttr) {
+            eval(onclickAttr);
         }
-    });
-
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true
     });
 })();
